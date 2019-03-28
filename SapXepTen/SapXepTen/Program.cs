1f3de5a27fee3662838530870048e3bf678c4f99
UYEN <huuuyen1998@gmail.com>
2019-03-28 10:32:44 +0700

    cach sap xep ko dung orderby

--- SapXepTen/SapXepTen/Program.cs
+++ SapXepTen/SapXepTen/Program.cs
@@ -12,45 +12,134 @@ namespace SapXepTen
         public int Tuoi { get; set; }
         public string GioiTinh { get; set; }
         public string Ten { get; set; }
+        public char[] kitu;
+
+        public HocSinh()
+        {
+
+        }
+
+        public HocSinh(String hoten, int tuoi, string gioitinh)
+        {
+            HoTen = hoten;
+            Tuoi = tuoi;
+            GioiTinh = gioitinh;
+        }
     }
 
     public class Program
     {
         public static void Main()
         {
-            List<HocSinh> mHocSinh = new List<HocSinh>()
+            List<HocSinh> Input = new List<HocSinh>()
             {
-
-            new HocSinh () { HoTen= "Nguyen Hoang Nam", Tuoi = 18, GioiTinh="nam"},
-            new HocSinh () { HoTen= "Le Tuan Anh", Tuoi =5, GioiTinh = "nam"},
-            new HocSinh () { HoTen= "Nguyen Thi Huyen",Tuoi = 10, GioiTinh= "nu"},
-            new HocSinh () { HoTen= "Vu Binh Duong", Tuoi =16,GioiTinh= "nam"},
-            new HocSinh () { HoTen= "Pham Thi Hue",Tuoi = 20,GioiTinh= "nu"},
-            new HocSinh () { HoTen= "Nguyen Thi Chinh",Tuoi = 11,GioiTinh= "nu"},
-            new HocSinh () { HoTen= "Le Thi Hoai",Tuoi = 10,GioiTinh= "nu"},
-            new HocSinh () { HoTen= "Phan Thi Hong Hao",Tuoi = 18,GioiTinh= "nu"},
-            new HocSinh () { HoTen= "Pham Duc Huy",Tuoi = 5,GioiTinh= "nam"},
-            new HocSinh () { HoTen= "Nguyen Thi Huyen",Tuoi = 8,GioiTinh= "nu"},
-            new HocSinh () { HoTen= "Pham Thi Chinh",Tuoi = 22,GioiTinh= "nu"},
+                new HocSinh ("Nguyen Van X",14,"nam"),
+                new HocSinh ("Nguyen Hoang Nam",18,"nam"),
+                new HocSinh ("Dinh Tuan Anh",5, "nam"),
+                new HocSinh ("Bui Thi Huyen",10,"nu"),
+                new HocSinh ("Vu Binh Duong",16, "nam"),
+                new HocSinh ("Pham Thi Hue", 20,"nu"),
+                new HocSinh ("Nguyen Thi Chinh",11, "nu"),
+                new HocSinh ("Phan Thi Hong",10, "nu"),
+                new HocSinh ("Phan Thi Hong Hao", 18, "nu"),
+                new HocSinh ("Pham Duc Huy",5, "nam"),
+                new HocSinh ("Nguyen Thi Huyen", 8, "nu"),
+                new HocSinh ("Pham Thi Chinh",22, "nu"),
             };
 
             //Cat lay ten de sap xep theo ten
-            foreach (HocSinh HS in mHocSinh)
+            foreach (HocSinh HS in Input)
             {
                 string[] cut = HS.HoTen.Split(' ');
                 HS.Ten = cut[cut.Length - 1];
+
+                Char[] arr = HS.HoTen.ToCharArray(0, HS.HoTen.Length);
+                HS.kitu = arr;
             }
             //Sap xep
-            List<HocSinh> sd = mHocSinh.OrderBy(x => x.Ten).ThenBy(x => x.Tuoi).ToList();
+
+            //List<HocSinh> Output = new List<HocSinh>();
+
+            //for (int i = 0; i< Input.Count; i++)
+            //{
+            //    int x = Input[i].Tuoi;
+            //    int dem = -1;
+            //    for ( int j=0; j<Output.Count;j++)
+            //    {
+            //        int y = Output[j].Tuoi;
+            //        if(x < y)
+            //        {
+            //            dem = j;
+            //            break;
+            //        }
+            //    }
+            //    if (dem>=0)
+            //    {
+            //        Output.Insert(dem, Input[i]);
+            //    }
+            //    else
+            //    {
+            //        Output.Add(Input[i]);
+            //    }
+            //}
+
+            //for (int i = 0; i<Input.Count; i++)
+            //{
+            //    for (int j=i+1; j<Input.Count; j++)
+            //    {
+            //        if (Input[i].Tuoi>Input[j].Tuoi)
+            //        {
+            //            HocSinh tg = Input[i];
+            //            Input[i] = Input[j];
+            //            Input[j] = tg;
+            //        }
+            //    }
+            //}
+
+            for (int i = 0; i < Input.Count; i++)
+            {
+                for (int j = i + 1; j < Input.Count; j++)
+                {
+                    if (Input[i].kitu[0] > Input[j].kitu[0])
+                    {
+                        HocSinh tg = Input[i];
+                        Input[i] = Input[j];
+                        Input[j] = tg;
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < Input.Count; i++)
+            {
+                for (int j = i + 1; j < Input.Count; j++)
+                {
+                    for (int v = 1; v < Input[i].kitu.Length && v < Input[j].kitu.Length; v++)
+                    {
+                        if (Input[i].kitu[v - 1] == Input[j].kitu[v - 1])
+                        {
+                            if (Input[i].kitu[v] > Input[j].kitu[v])
+                            {
+                                HocSinh tg = Input[i];
+                                Input[i] = Input[j];
+                                Input[j] = tg;
+                                break;
+                            }
+                        }
+                        else break;
+
+                    }
+                }
+            }
+
+            //List<HocSinh> sd = mHocSinh.OrderBy(x => x.HoTen).ToList();
 
             Console.WriteLine("\nDanh sach hoc sinh: ");
-            foreach (HocSinh HS in sd)
+            foreach (HocSinh HS in Input)
             {
                 Console.WriteLine("\n" + HS.HoTen + " ....... " + HS.Tuoi + " ....... " + HS.GioiTinh);
             }
             Console.ReadKey();
-
-
         }
     }
 }
